7152f900c86887f2f83772fe567a50140838f269
lucky-sharma-oz <luckysharma13032006@gmail.com>
2026-06-08 09:00:35 +0530

    Create Reverse_string.cpp

---
+++ Week_2/Day_1/Reverse_string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int L = 0;
+        int R = s.size() - 1;
+
+        while(L < R){
+            swap(s[L++], s[R--]);
+        }
+    }
+};
