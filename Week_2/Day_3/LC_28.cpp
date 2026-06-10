7e90a7af6e024cc64071bd309ebe639e9dcda5a8
lucky-sharma-oz <luckysharma13032006@gmail.com>
2026-06-10 23:32:02 +0530

    Create LC_28.cpp

---
+++ Week_2/Day_3/LC_28.cpp
@@ -0,0 +1,27 @@
+//Find the Index of the First Occurrence in a String
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+
+        for(int i = 0; i < haystack.length(); i++){
+
+            bool found = true;
+
+           for(int j = 0; j < needle.length(); j++){
+
+            if(haystack[i+j] != needle[j]){
+                
+                found = false;
+                break;
+            }
+
+           }
+            
+            if(found) return i;
+        }
+        return -1;
+        
+    }
+};
